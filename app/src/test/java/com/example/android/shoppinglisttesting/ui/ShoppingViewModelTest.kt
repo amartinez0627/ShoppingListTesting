c374052a374eaa44e14432157a029077e32fbe7a
Antonio Martinez <antoniomartinez12347@gmail.com>
2023-05-23 15:26:21 -0600

    created unit tests for viewModel

---
+++ app/src/test/java/com/example/android/shoppinglisttesting/ui/ShoppingViewModelTest.kt
@@ -0,0 +1,111 @@
+package com.example.android.shoppinglisttesting.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.shoppinglisttesting.MainCoroutineRule
+import com.example.android.shoppinglisttesting.commons.Constants
+import com.example.android.shoppinglisttesting.commons.Status
+import com.example.android.shoppinglisttesting.getOrAwaitValueTest
+import com.example.android.shoppinglisttesting.repositories.FakeShoppingRepository
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class ShoppingViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule= MainCoroutineRule()
+
+    private lateinit var viewModel: ShoppingViewModel
+
+    @Before
+    fun setup(){
+        viewModel = ShoppingViewModel(FakeShoppingRepository())
+    }
+
+    @Test
+    fun `insert shopping item with empty field, returns error`(){
+        viewModel.insertShoppingItem("name","","3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too long name, returns error`() {
+        val string = buildString {
+            for (i in 1..Constants.MAX_NAME_LENGTH + 1){
+                append(1)
+            }
+        }
+
+        viewModel.insertShoppingItem(string,"5","3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too long price, returns error`(){
+        val string = buildString {
+            for (i in 1..Constants.MAX_PRICE_LENGTH + 1){
+                append(1)
+            }
+        }
+
+        viewModel.insertShoppingItem("name","5",string)
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too high amount, returns error`(){
+
+        viewModel.insertShoppingItem("name","999999999999999999","3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with valid input, returns success`(){
+
+        viewModel.insertShoppingItem("name","5","3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.SUCCESS)
+    }
+    @Test
+    fun `setCurrentImageUrl() should change _currentImageUrl value`(){
+        val testPath = "testPath"
+
+        viewModel.setCurrentImageUrl(testPath)
+
+        val value = viewModel.currentImageUrl.getOrAwaitValueTest()
+
+        assertThat(value).isEqualTo(testPath)
+    }
+
+    @Test
+    fun `insertShoppingItem() success should change _currentImageUrl value to empty`(){
+        val testPath = "testPath"
+
+        viewModel.setCurrentImageUrl(testPath)
+        viewModel.insertShoppingItem("name","5","3.0")
+
+        val value = viewModel.currentImageUrl.getOrAwaitValueTest()
+
+        assertThat(value).isEmpty()
+    }
+}
