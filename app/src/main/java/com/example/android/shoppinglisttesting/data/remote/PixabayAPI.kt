16e8e042796154136accd179c707d6759bf3a95c
Antonio Martinez <antoniomartinez12347@gmail.com>
2023-05-22 20:28:13 -0600

    Dependency injection and Room was set up.

---
+++ app/src/main/java/com/example/android/shoppinglisttesting/data/remote/PixabayAPI.kt
@@ -0,0 +1,16 @@
+package com.example.android.shoppinglisttesting.data.remote
+
+import com.example.android.shoppinglisttesting.BuildConfig
+import com.example.android.shoppinglisttesting.data.remote.responses.ImageResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PixabayAPI {
+
+    @GET("/api/")
+    suspend fun searchForImage(
+        @Query("q") searchQuery: String,
+        @Query("key") apiKey: String = BuildConfig.API_KEY
+    ): Response<ImageResponse>
+}
