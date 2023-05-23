05ba307da0dcf2ec29cc5a8ff43c6298fb85de9a
Antonio Martinez <antoniomartinez12347@gmail.com>
2023-05-22 19:32:51 -0600

    implemented database and tested its DAOs

---
+++ app/src/main/java/com/example/android/shoppinglisttesting/data/local/ShoppingDao.kt
@@ -0,0 +1,20 @@
+package com.example.android.shoppinglisttesting.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertShoppingItem(item: ShoppingItem)
+
+    @Delete
+    suspend fun deleteSoppingItem(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun observeAllShoppingItems(): LiveData<List<ShoppingItem>>
+
+    @Query("SELECT SUM(price * amount) FROM shopping_items")
+    fun observeTotalPrice(): LiveData<Float>
+}
