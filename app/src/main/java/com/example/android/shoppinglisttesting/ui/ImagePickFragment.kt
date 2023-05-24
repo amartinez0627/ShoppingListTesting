64a3e03864ab67861af600f347a925a49ba23020
Antonio Martinez <antoniomartinez12347@gmail.com>
2023-05-24 17:53:11 -0600

    added search image functionality

--- app/src/main/java/com/example/android/shoppinglisttesting/ui/ImagePickFragment.kt
+++ app/src/main/java/com/example/android/shoppinglisttesting/ui/ImagePickFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.shoppinglisttesting.R
 import com.example.android.shoppinglisttesting.adapters.ImageAdapter
 import com.example.android.shoppinglisttesting.commons.Constants.GRID_SPAN_COUNT
+import com.example.android.shoppinglisttesting.commons.Constants.SEARCH_TIME_DELAY
+import com.example.android.shoppinglisttesting.commons.Status
 import com.example.android.shoppinglisttesting.databinding.FragmentImagePickBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -38,6 +46,21 @@ class ImagePickFragment @Inject constructor(
         viewModel = ViewModelProvider(requireActivity())[ShoppingViewModel::class.java]
 
         setupRecyclerView()
+        subscribeToObservers()
+
+        var job: Job? = null
+
+        binding.etSearch.addTextChangedListener { editable->
+            job?.cancel()
+            job = lifecycleScope.launch {
+                delay(SEARCH_TIME_DELAY)
+                editable?.let {
+                    if(editable.toString().isNotEmpty()){
+                        viewModel.searchForImage(editable.toString())
+                    }
+                }
+            }
+        }
 
         imageAdapter.setOnItemClickListener { url ->
             findNavController().popBackStack()
@@ -49,6 +72,31 @@ class ImagePickFragment @Inject constructor(
         binding.rvImages.apply {
             adapter = imageAdapter
             layoutManager = GridLayoutManager(requireContext(), GRID_SPAN_COUNT)
+        }
+    }
+
+    private fun subscribeToObservers(){
+        viewModel.images.observe(viewLifecycleOwner){
+            it?.getContentIfNotHandled()?.let { result ->
+                when(result.status){
+                    Status.SUCCESS -> {
+                        val urls = result.data?.hits?.map { imageResult ->  imageResult.previewURL}
+                        imageAdapter.images = urls ?: listOf()
+                        binding.progressBar.visibility = View.GONE
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            requireView(),
+                            result.message ?: "An unknown error occurred.",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        binding.progressBar.visibility = View.GONE
+                    }
+                    Status.LOADING -> {
+                        binding.progressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
         }
     }
 
--- app/src/main/java/com/example/android/shoppinglisttesting/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/shoppinglisttesting/ui/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.android.shoppinglisttesting.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.android.shoppinglisttesting.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory = fragmentFactory
+
         setContentView(R.layout.activity_main)
     }
 }
