a62478859aab4999f96a1d74fee70b246d7911c6
Antonio Martinez <antoniomartinez12347@gmail.com>
2023-05-23 18:47:41 -0600

    Added functions to test fragments

---
+++ app/src/debug/java/com/example/android/shoppinglisttesting/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.example.android.shoppinglisttesting
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
